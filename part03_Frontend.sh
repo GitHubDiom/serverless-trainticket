1c3c47d8222de94e548c58743e630fa2c11dfe73
GitHubDiom <867320164@qq.com>
2023-02-21 14:39:38 +0000

    minor: build container image before deployment

--- part03_Frontend.sh
+++ part03_Frontend.sh
@@ -1,7 +1,10 @@
 echo "Part03 Front Deployment"
-
+cd src/frontend
+bash build-image.sh
+cd -
 cd deployment/Part03-frontend/
-kubectl apply -f ts-serverless-frontend-deployment.yml
+kubectl delete -f ts-serverless-frontend-deployment.yml
+kubectl create -f ts-serverless-frontend-deployment.yml
 
 cd ..
 cd ..
