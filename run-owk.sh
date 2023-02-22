05c3c665cee206ae01f84e9d18a3ac8d960f0b18
GitHubDiom <867320164@qq.com>
2023-02-22 12:13:56 +0000

    minor: update gitignore and auto-deploy script

---
+++ run-owk.sh
@@ -0,0 +1,12 @@
+./part01_DataBaseDeployment_owk.sh
+sleep 20
+./part01_DataInitiation_owk.sh
+sleep 20
+
+./part02_BaaSServices.sh
+sleep 20
+./part02_FaaSFunctions_owk.sh
+sleep 20
+
+./part03_Frontend.sh
+sleep 20
