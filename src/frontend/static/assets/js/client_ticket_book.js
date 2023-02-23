afe06140d78f4d5316969d85e2506ce518a37cb7
GitHubDiom <867320164@qq.com>
2023-02-23 02:02:24 +0000

    fix: change contentType to json

--- src/frontend/static/assets/js/client_ticket_book.js
+++ src/frontend/static/assets/js/client_ticket_book.js
@@ -379,7 +379,7 @@ function preserveCreateNewContacts() {
         type: "post",
         // url: "/function/create-new-contacts.openfaas-fn",
         url: "/api/23bc46b1-71f6-4ed5-8c54-816aa4f8c502/contact/createNewContacts",
-        contentType: "text/plain",
+        contentType: "application/json",
         headers: {"Authorization": "Bearer " + sessionStorage.getItem("client_token")},
         dataType: "json",
         data: data,
