d17d2f05cc07e0936965aea329506a74523f6e00
GitHubDiom <867320164@qq.com>
2023-02-27 03:22:54 +0000

    minor: change the way of lauching the invoker.py

--- dev-support/getTripAllDetailInfo/handler.py
+++ dev-support/getTripAllDetailInfo/handler.py
@@ -7,9 +7,9 @@ INIT_SCRIPT_PATH=f"{SCRIPT_PATH}/init.sh"
 RUN_SCRIPT_PATH=f"{SCRIPT_PATH}/run.sh"
 
 # ./invoke.py init -main Handler $JAR_PATH
-init_output=sp.check_output(f"../invoke.py init -main Handler {load_dict['JAR_PATH']}", shell=True)
+init_output=sp.check_output(f"python3 ../invoke.py init -main Handler {load_dict['JAR_PATH']}", shell=True)
 print(f"init output:\n{init_output.decode('utf-8')}")
 
 # ./invoke.py run $INPUT_STRING
-run_output=sp.check_output(f"../invoke.py run '{json.dumps(load_dict['INPUT_STRING'])}'", shell=True)
+run_output=sp.check_output(f"python3 ../invoke.py run '{json.dumps(load_dict['INPUT_STRING'])}'", shell=True)
 print(f"invoke output:\n{run_output.decode('utf-8')}")
